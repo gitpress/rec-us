4d845c7ebc45c0ae6e9a376e946044a379ea484a
anotherangrymouse <grillopress.com@gmail.com>
2014-09-18 09:48:43 +0000

    add window callback to jquery image position code

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 /***********************************************
- * 
+ * jQuery for Recruitment Site
  * ******************************************** */
 
 $(document).ready(function() {
-  
-	if ($(window).width() < 700) {
-   $( ".responsive-media" ).removeClass( "pull-left" );
+
+  if ($(window).width() < 700) {
+    $(".responsive-media").removeClass("pull-left");
   }
+
+  //simple function to check if the screensize changes and to add or remove a class. IE safe
   
+  $( window ).resize(function() {
+    if ($(window).width() > 700){
+      $(".responsive-media").addClass("pull-left");
+    }
+    else {
+      $(".responsive-media").removeClass("pull-left");
+    };
+  });
+  
+  
+
 });
 
 
