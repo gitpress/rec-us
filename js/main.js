1a4687d7239740b132ca4b2022f0aec82f1af54b
anotherangrymouse <grillopress.com@gmail.com>
2014-09-19 08:38:33 +0000

    improve jQuery selector by adding ID context

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@
 $(document).ready(function() {
 
   if ($(window).width() < 700) {
-    $(".responsive-media").removeClass("pull-left");
+    $(".responsive-media",$("#nurse-why-sth")).removeClass("pull-left");
   }
 
   //simple function to check if the screensize changes and to add or remove a class
 
   $(window).resize(function() {
     if ($(window).width() > 700) {
-      $(".responsive-media").addClass("pull-left");
+      $(".responsive-media",$("#nurse-why-sth")).addClass("pull-left");
     } else {
-      $(".responsive-media").removeClass("pull-left");
+      $(".responsive-media",$("#nurse-why-sth")).removeClass("pull-left");
     };
   });
 
