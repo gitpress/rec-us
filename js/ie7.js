81249381c4ff4ab52f4e854937581fee80ea12ff
GrilloPress <grillopress@gmail.com>
2015-04-14 12:18:20 +0000

    add reg nurse event information

---
+++ js/ie7.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  "use strict";
+  
+  // this basic example just reduces to columns which
+  // aligns things in IE7. This is a "dumb" version that should
+  // solve most cases simply with 7 lines of jquery
+  $( ".col-md-4" ).addClass( "col-md-3" );
+  $( ".col-md-5" ).addClass( "col-md-4" );
+  $( ".col-md-6" ).addClass( "col-md-5" );
+  $( ".col-md-8" ).addClass( "col-md-6" );
+  $( ".col-md-12" ).addClass( "col-md-10" );
+  
+  // two functions... first action the reduction of columns... then a second function that makes everything offset by 1
+  
+  
+});
